03eda0e70c3eb46a6cbbd6d346ee51dc9ac2c47f
jy91 <zjyzjy56@163.com>
2023-07-22 21:20:45 +0800

    第二次提交

--- test.py
+++ test.py
@@ -1,15 +1,21 @@
+import random
+
+
 class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
+    num = random.randint(1, 10)
+
     def eat(self):
         print('{} is eating'.format(self.name))
+        print('你的年龄是{}'.format(self.age))
 
-    def run(self):
+    def run(self, num):
         print('{} is running'.format(self.name))
 
 
 P1 = Person('张三',22)
 P1.eat()
-P1.run()
+P1.run(2)
